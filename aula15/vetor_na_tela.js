5c30fe4c4e81516907652e2fbd71f95d35440d9a
JeanCarlosHP <jeanchpaula@gmail.com>
2021-06-05 16:47:09 -0300

    Arrays | Vetores

---
+++ aula15/vetor_na_tela.js
@@ -0,0 +1,10 @@
+let valores = [8, 1, 7, 4, 2, 9]
+
+// for (let i = 0; i < valores.length; i++) {
+//     console.log(valores[i])
+// }
+console.log(`Valor no index: ${valores.indexOf(7)}\n`)
+
+for (let i in valores) {
+    console.log(valores[i])
+}
